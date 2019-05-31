ad6307874e6cc19c26978c55ff294d66f43f12bb
Gabriel Ewig <gabriel@cyclikal.com>
2019-05-31 13:59:46 -0500

    Increment to dev4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cyckei",
-    version="0.0.1.dev3",
+    version="0.0.1.dev4",
     author="Gabriel Ewig",
     author_email="gabriel@cyclikal.com",
     description="Keithley Battery Cycler for Python3",
