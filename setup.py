213b2829e0e6efb9044fb8c4c587617c47c0af22
Gabriel Ewig <gabriel@cyclikal.com>
2020-01-16 23:30:24 -0500

    Load setup.py vars from file

--- setup.py
+++ setup.py
@@ -1,28 +1,24 @@
 from setuptools import setup
+import json
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
+with open("README.rst", "r") as file:
+    long_description = file.read()
+with open("cyckei/assets/variables.json", "r") as file:
+    var = json.load(file)
 
 setup(
-    name="cyckei",
-    version="0.3.dev1",
-    author="Cyclikal, LLC.",
-    author_email="gabriel@cyclikal.com",
-    description="Keithley Battery Cycler for Python 3",
+    name=var["name"],
+    version=var["version"],
+    author=var["author"],
+    author_email=var["email"],
+    description=var["description"],
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    keywords='keithley battery lithium-ion cycling calorimetry',
-    url="https://gitlab.com/cyclikal/cyckei",
+    keywords=var["keywords"],
+    url=var["url"],
 
     packages=["cyckei", "cyckei.server", "cyckei.client", "cyckei.explorer",
               "cyckei.functions"],
-    package_dir={
-        "cyckei": "cyckei",
-        "server": "cyckei/server",
-        "client": "cyckei/client",
-        "explorer": "cyckei/explorer",
-        "functions": "cyckei/functions"
-    },
     include_package_data=True,
 
     classifiers=[
@@ -32,7 +28,7 @@ setup(
         "Topic :: Scientific/Engineering",
     ],
     install_requires=[
-        "zmq",
+        "pyzmq",
         "pyvisa",
         "PySide2",
         "matplotlib",
