f95a1abf156928b39cddcd731da5ef201c5947a2
Gabriel Ewig <gabriel@cyclikal.com>
2020-03-19 17:44:03 -0400

    Leave note on NI-GPIB-USB-HS

---
+++ cyckei/assets/ni-gpib-usb-hs.py
@@ -0,0 +1,5 @@
+"""
+This plugin is intended to contain instructions for NI-USB-GPIB-HS devices.
+This is the device currently used as default by Cyckei.
+Therefore, it is not necessary that this be populated or integrated.
+"""
