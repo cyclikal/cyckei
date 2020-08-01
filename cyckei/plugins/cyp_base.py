505a6ec847005fe313ca214852821d46fbf50e75
Gabriel Ewig <gabriel@cyclikal.com>
2020-08-01 13:07:20 -0500

    Fix logging

--- cyckei/plugins/cyp_base.py
+++ cyckei/plugins/cyp_base.py
@@ -1,7 +1,5 @@
 import logging
 
-logger = logging.getLogger("cyckei")
-
 
 class PluginController(object):
     """
@@ -9,24 +7,32 @@ class PluginController(object):
     Creates default methods for interacting with plugin and handling sources.
     """
 
-    def __init__(self):
+    def __init__(self, name):
         """Setup logging and sources for plugin."""
-
         # Check if "cyckei" logger found, and setup seperate handler if not.
-        self.check_logger()
-        self.sources = self.get_sources()
+        self.name = name
+        self.logger = self.get_logger()
 
-    def check_logger(self):
+    def get_logger(self):
         """
         Plugin initially tries to connect to to Cyckei's main loggin handlers.
         If this fails, this method establishes a new console handler.
         Usually this should be as a result of running the plugin independantly.
         """
 
-        if not logger.hasHandlers():
-            c_handler = logging.StreamHandler()
-            c_handler.setLevel(logging.DEBUG)
-            logger.addHandler(c_handler)
+        logger = logging.getLogger(self.name)
+        logger.setLevel(logging.DEBUG)
+
+        # Setting individual handlers and logging levels
+        c_handler = logging.StreamHandler()
+
+        c_handler.setLevel(logging.DEBUG)
+        # f_handler.setLevel(logging.ERROR)
+
+        # Add handlers to the logger
+        logger.addHandler(c_handler)
+        # logger.addHandler(f_handler)
+        return logger
 
     def get_sources(self):
         """
@@ -39,7 +45,7 @@ class PluginController(object):
 
         raise NotImplementedError
 
-    def read(self, source=None):
+    def read(self, source):
         try:
             return self.sources[source].read()
         except (TypeError, KeyError) as e:
