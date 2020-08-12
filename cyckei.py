4f6fa7b954f27e5981079a8efe7b65b1cf6230ad
Gabriel Ewig <gabriel@cyclikal.com>
2020-08-12 11:44:50 -0500

    Force close on KeyboardInterrupt

--- cyckei.py
+++ cyckei.py
@@ -1,4 +1,15 @@
 """This file is for execution from the command line via 'python cyckei.py'."""
 from cyckei import cyckei
+import sys
+import os
 
-cyckei.main()
+if __name__ == '__main__':
+    try:
+        cyckei.main()
+    except KeyboardInterrupt as error:
+        print(f"Got keyboard interrupt: {error}")
+        try:
+            sys.exit(0)
+        except SystemExit as error:
+            print(f"sys.exit failed: {error}")
+            os._exit(0)
