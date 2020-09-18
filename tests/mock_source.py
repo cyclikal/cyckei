49d56bba2cbecd1c9dde4b41aedda01aeeb6e818
Vincent Chevrier <vincentchevrier@gmail.com>
2020-09-17 22:47:22 -0500

    mock of a source object

---
+++ tests/mock_source.py
@@ -0,0 +1,44 @@
+import numpy as np
+import time
+
+
+class MockSource():
+    w = 100.  # mAh
+    s = -1.  # shift to avoid dorppping too low in voltage
+    R = 2.  # internal resistance in Ohms
+
+    def __init__(self):
+        self.current = 0
+        self.t0 = 0
+        self.x0 = 0
+
+    def nernst(self):
+        mAh = self.capacity()
+        return 3.7+0.2*(np.log(1./(1./(mAh-self.s)-1./(self.w-self.s)))-np.log(self.w))+self.current*self.R
+
+    def capacity(self, reset=False):
+        t = time.time()
+        dt = t-self.t0
+        mAh = self.x0 + (self.current*1000.)*(dt/3600.)
+        if reset:
+            self.t0 = t
+            self.x0 = mAh
+
+        return mAh
+
+    def read_iv(self):
+        return self.current, self.nernst()
+
+    def off(self):
+        self.capacity(reset=True)
+        self.current = 0
+
+    def pause(self):
+        self.off()
+
+    def rest(self):
+        self.off
+
+    def set_current(self, current=0, v_limit=None):
+        self.t0 = time.time()
+        self.current = current
